22b82692e5428b4174ee6e6fdd5f9294b775edd2
Muhil Varnan <muhilvarnan.v@thoughtworks.com>
2024-01-04 20:02:26 +0530

    moved collection name to env

--- src/config.py
+++ src/config.py
@@ -3,3 +3,7 @@ import os
 
 def get_database_connection_url():
     return os.getenv("DATABASE_URL")
+
+
+def get_collection_name():
+    return os.getenv("COLLECTION_NAME")
--- src/vector_store.py
+++ src/vector_store.py
@@ -1,12 +1,12 @@
 from langchain.vectorstores.pgvector import PGVector
 from langchain.embeddings.vertexai import VertexAIEmbeddings
 
-from src.config import get_database_connection_url
+from src.config import get_database_connection_url, get_collection_name
 
 embeddings = VertexAIEmbeddings(model_name="textembedding-gecko@001")
 
 store = PGVector(
     embedding_function=embeddings,
-    collection_name="knowledge_base",
+    collection_name=get_collection_name(),
     connection_string=get_database_connection_url(),
 )
